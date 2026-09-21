ac715001ae04d695b182306fa80ecbc7c5a08dd3
Вячеслав <p0chta3357@yandex.ru>
2026-09-21 17:36:28 +0300

    Implement Student mapper

--- src/main/java/com/example/student/mapper/StudentMapper.java
+++ src/main/java/com/example/student/mapper/StudentMapper.java
@@ -2,13 +2,24 @@ package com.example.student.mapper;
 
 import com.example.student.dto.StudentDto;
 import com.example.student.model.Student;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public class StudentMapper {
-    public <studentDto> List<studentDto> toDtoList(List<Student> all) {
-        StudentDto dto=new
+    public StudentDto toDto(Student student) {
+        return new StudentDto(student.getId(), student.getName(), student.getSurname());
+    }
 
-        return dto;
+    public List<StudentDto> toDtoList(List<Student> students) {
+        return students.stream().map(this::toDto).toList();
+    }
+
+    public Student toEntity(StudentDto dto) {
+        Student student = new Student();
+        student.setName(dto.getName());
+        student.setSurname(dto.getSurname());
+        return student;
     }
 }

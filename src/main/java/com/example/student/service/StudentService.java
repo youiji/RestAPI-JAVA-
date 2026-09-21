c8d2e1c05c1a601a30131ca29397d3d18536cc1b
Вячеслав <p0chta3357@yandex.ru>
2026-09-21 17:36:31 +0300

    Normalize Student service interface

--- src/main/java/com/example/student/service/StudentService.java
+++ src/main/java/com/example/student/service/StudentService.java
@@ -1,11 +1,12 @@
 package com.example.student.service;
 
 import com.example.student.model.Student;
+
 import java.util.List;
 
 public interface StudentService {
     List<Student> findAll();
     Student findById(Long id);
     Student save(Student student);
-    void DeleteById(Long id);
+    void deleteById(Long id);
 }
